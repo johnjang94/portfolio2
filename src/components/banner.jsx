00c3cc358427c18dc08196d563d54acc2fdae7be
John Jang <jscolbe9675@gmail.com>
2024-05-18 16:06:58 -0400

    minor changes #5

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,14 +1,34 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-// Photos
-import Logo from "../../public/logo500.png";
+// Import logos
+import LogoTransparent from "../../public/logo500.png";
+import LogoBlack from "../../public/logo512.png";
 
 export default function Banner() {
+  const location = useLocation();
+
+  const getBackgroundColor = () => {
+    switch (location.pathname) {
+      case "/televu":
+      case "/development":
+        return "bg-black text-white";
+      default:
+        return "bg-transparent text-black";
+    }
+  };
+
+  // Conditionally select the logo based on background color
+  const logo = getBackgroundColor().includes("bg-black")
+    ? LogoTransparent
+    : LogoBlack;
+
   return (
-    <div className="flex justify-between items-center px-10 py-10 bg-black text-white">
-      <img src={Logo} width={30} alt="Logo" />
+    <div
+      className={`flex justify-between items-center px-10 py-10 ${getBackgroundColor()} text-white`}
+    >
+      <img src={logo} width={30} alt="Logo" />
       <div className="flex space-x-1">
         <span className="text-blue-500">&copy;</span>
         <div className="md:flex mt-1 md:mt-0 md:space-x-1">
