c2f0b75ad46a707cbf712da34b4fa9372d021138
John Jang <jscolbe9675@gmail.com>
2024-05-29 16:16:03 -0400

    updated design section at home page

--- src/pages/home/designProject.jsx
+++ src/pages/home/designProject.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 
 // Photos
-import TeleVU from "../../assets/design/televu.png";
+import FoodDistro from "../../assets/design/foodDistro.png";
 
 export default function DesignHome() {
   return (
     <div className="my-64">
       <h1 className="text-2xl md:text-4xl text-center">DESIGN AT THE LATEST</h1>
-      <h2 className="text-7xl md:text-9xl my-2 text-center">TELEVU</h2>
-      <img src={TeleVU} alt="TeleVU" width={650} className="mx-auto" />
+      <h2 className="text-7xl md:text-9xl my-2 text-center">FOOD DISTRO</h2>
+      <img
+        src={FoodDistro}
+        alt="Food Distro logo"
+        width={650}
+        className="mx-auto"
+      />
       {/* I need to center the following button */}
       <div className="text-center">
         <Link to="/televu">
