6f8c0ae993ed3e7bd2458ca2701f42559107e503
John Jang <jscolbe9675@gmail.com>
2024-06-03 09:40:00 -0400

    correcting vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/contact": {
+        target: "http://portfolio-contact.us-east-1.elasticbeanstalk.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/contact/, "/contact"),
+      },
+    },
+  },
 });
