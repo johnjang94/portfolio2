443e1684eb7c6bcf21436dc7b911b4f19236419e
John Jang <jscolbe9675@gmail.com>
2024-06-19 09:07:58 -0400

    need to correct the navigation CSS

--- src/components/mainNav.jsx
+++ src/components/mainNav.jsx
@@ -50,105 +50,122 @@ export default function Nav({ pathname }) {
     pathname === "/lock" || isNotRegistered() ? "text-white" : "text-black";
 
   return (
-    <div
-      className={`px-10 justify-between flex py-5 border-b border-gray-300 items-center ${textColor}`}
-    >
-      <div className="hidden md:block">
-        <Link to="/home">
-          <img src={logoSrc} alt="Logo" width={30} />
-        </Link>
-      </div>
-      <button onClick={toggleMenu} className="block md:hidden">
-        <RxHamburgerMenu className={`text-2xl ${textColor}`} />
-      </button>
-      {/* Menu */}
-      {isMenuOpen && (
-        <div
-          className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50"
-          onClick={closeMenu}
-        >
+    <>
+      <div
+        className={`px-10 flex justify-between w-full py-5 border-b border-gray-300 items-center ${textColor} hidden md:block`}
+      >
+        <div>
+          <button onClick={toggleMenu}>
+            <RxHamburgerMenu className={`text-2xl ${textColor}`} />
+          </button>
+        </div>
+        {/* Menu */}
+        {isMenuOpen && (
           <div
-            className={`fixed inset-y-0 left-0 md:max-w-xs w-full max-h-screen bg-emerald-200 shadow overflow-y-scroll md:overflow-auto ${
-              isMenuOpen ? "slide-in" : "slide-out"
-            }`}
+            className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50"
+            onClick={closeMenu}
           >
-            <button className="float-right px-10 pt-10" onClick={closeMenu}>
-              &#10060;
-            </button>
-            <ul className="py-24 px-10 space-y-5">
-              <div
-                className={`${
-                  isMenuOpen ? "home-slide-in" : ""
-                } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
-              >
-                <Link to="/home">
-                  <img
-                    src={Home}
-                    alt="Home"
-                    width={300}
-                    className="md:hidden rounded-xl mx-auto"
-                  />
+            <div
+              className={`fixed inset-y-0 left-0 md:max-w-xs w-full max-h-screen bg-emerald-200 shadow overflow-y-scroll md:overflow-auto ${
+                isMenuOpen ? "slide-in" : "slide-out"
+              }`}
+            >
+              <button className="float-right px-10 pt-10" onClick={closeMenu}>
+                &#10060;
+              </button>
+              <ul className="py-24 px-10 space-y-5">
+                <div
+                  className={`${
+                    isMenuOpen ? "home-slide-in" : ""
+                  } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
+                >
+                  <Link to="/home">
+                    <img
+                      src={Home}
+                      alt="Home"
+                      width={300}
+                      className="md:hidden rounded-xl mx-auto"
+                    />
 
-                  <button className="p-2">Home</button>
-                </Link>
-              </div>
-              <div
-                className={`${
-                  isMenuOpen ? "about-slide-in" : ""
-                } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
-              >
-                <Link to="/about">
-                  <img
-                    src={Profile}
-                    alt="Profile picture"
-                    width={300}
-                    className="md:hidden mx-auto"
-                  />
+                    <button className="p-2">Home</button>
+                  </Link>
+                </div>
+                <div
+                  className={`${
+                    isMenuOpen ? "about-slide-in" : ""
+                  } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
+                >
+                  <Link to="/about">
+                    <img
+                      src={Profile}
+                      alt="Profile picture"
+                      width={300}
+                      className="md:hidden mx-auto"
+                    />
 
-                  <button className="p-2">About Me</button>
-                </Link>
-              </div>
-              <div
-                className={`${
-                  isMenuOpen ? "design-slide-in" : ""
-                } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
-              >
-                <Link to="/design">
-                  <img
-                    src={Design}
-                    alt="design picture"
-                    width={300}
-                    className="md:hidden mx-auto"
-                  />
+                    <button className="p-2">About Me</button>
+                  </Link>
+                </div>
+                <div
+                  className={`${
+                    isMenuOpen ? "design-slide-in" : ""
+                  } text-3xl text-end sm:text-base sm:text-start border-white-2 hover:bg-slate-300 sm:hover:bg-transparent sm:hover:text-blue-400 hover:cursor-pointer rounded-3xl`}
+                >
+                  <Link to="/design">
+                    <img
+                      src={Design}
+                      alt="design picture"
+                      width={300}
+                      className="md:hidden mx-auto"
+                    />
 
-                  <button className="p-2">UX Design</button>
-                </Link>
-              </div>
-            </ul>
+                    <button className="p-2">UX Design</button>
+                  </Link>
+                </div>
+              </ul>
+            </div>
+          </div>
+        )}
+        {/* Menu */}
+        <div>
+          <Link to="/home">
+            <img src={logoSrc} alt="Logo" width={30} />
+          </Link>
+        </div>
+        <div className="flex space-x-5 items-center">
+          <div className="block">
+            <Link to="/about">
+              <button>About Me</button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/contact">
+              <button>Contact</button>
+            </Link>
           </div>
         </div>
-      )}
-      {/* Menu */}
-      <div className="block md:hidden">
-        <Link to="/home">
-          <img src={logoSrc} alt="Logo" width={30} />
-        </Link>
       </div>
-      <div className="flex space-x-5 items-center">
-        <div className="hidden md:block">
-          <Link to="/about">
-            <button>About Me</button>
+      <div
+        className={`px-10 justify-between flex py-5 border-b border-gray-300 items-center ${textColor} block md:hidden`}
+      >
+        <div className="block">
+          <Link to="/home">
+            <img src={logoSrc} alt="Logo" width={30} />
           </Link>
         </div>
-        <div className="hidden">
-          <Link to="/contact">
-            <button>Contact</button>
-          </Link>
+        <div className="flex space-x-5 items-center">
+          <div className="block">
+            <Link to="/about">
+              <button>About Me</button>
+            </Link>
+          </div>
+          <div>
+            <a href="mailto:jscolbe9675@gmail.com">
+              <button>Contact</button>
+            </a>
+          </div>
         </div>
-        <a href="mailto:jscolbe9675@gmail.com">
-          <button>Contact</button>
-        </a>
       </div>
-    </div>
+    </>
   );
 }
