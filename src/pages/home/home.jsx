36ceb824396eca1e273a7a0619c7ce3739741b2d
John Jang <jscolbe9675@gmail.com>
2024-07-30 11:51:09 -0400

    modified the home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -82,7 +82,7 @@ export default function Home() {
     <div>
       <header className="lg:my-24 md:mx-auto mx-8 lg:w-6/12 w-11/12 mt-20 mb-82 2xl:my-80">
         <h1 className="lg:text-9xl text-5xl">John Jang</h1>
-        <div className="rotating-text-container lg:mt-5 mt-5 lg:text-3xl text-3xl">
+        <div className="rotating-text-container lg:mt-5 mt-5 lg:text-2xl text-xl">
           {titles.map((title, index) => (
             <h2
               key={index}
@@ -93,8 +93,9 @@ export default function Home() {
             </h2>
           ))}
         </div>
-        <p className="mt-6 lg:text-3xl text-xl leading-relaxed">
-          Product Designer at <span className="font-bold">THE SAHKI</span>.
+        <p className="mt-6 lg:text-2xl text-xl leading-relaxed">
+          Currently, I&#39;m a Product Designer at{" "}
+          <span className="font-bold">THE SAHKI</span>.
         </p>
         <div className="md:flex justify-between">
           <Link
@@ -113,17 +114,27 @@ export default function Home() {
         {data.map((item) => (
           <div
             key={item.id}
-            className="w-fit transition-transform duration-300 ease-in-out hover:-translate-y-2 space-y-3"
+            className={`w-fit transition-transform duration-300 ease-in-out hover:-translate-y-2 space-y-3 p-2 rounded-xl`}
           >
-            <Link to={item.link}>
-              <img
-                src={item.imgSrc}
-                alt={item.imgAlt}
-                className={`rounded-xl hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,rgba(0,0,0,0.3)_0px_3px_7px_-3px] mx-auto md:h-[300px] ${
-                  item.id === 2 ? "w-auto" : "w-full"
-                }`}
-              />
-            </Link>
+            <div
+              className={`p-3 rounded-xl hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,rgba(0,0,0,0.3)_0px_3px_7px_-3px] ${
+                item.id === 1
+                  ? "bg-[radial-gradient(circle_311px_at_8.6%_27.9%,rgba(62,147,252,0.57)_12.9%,rgba(239,183,192,0.44)_91.2%)]"
+                  : item.id === 2
+                  ? "bg-[radial-gradient(_circle_farthest-corner_at_10%_20%,rgba(255,209,67,1)_0%,rgba(255,145,83,1)_90%_)]"
+                  : item.id === 3
+                  ? "bg-[radial-gradient(_circle_farthest-corner_at_48.4%_47.5%,rgba(122,183,255,1)_0%,rgba(21,83,161,1)_90%_)]"
+                  : "bg-slate-400"
+              }`}
+            >
+              <Link to={item.link}>
+                <img
+                  src={item.imgSrc}
+                  alt={item.imgAlt}
+                  className="rounded-xl mx-auto md:h-[300px]"
+                />
+              </Link>
+            </div>
             <p className="font-bold">
               {item.icon} {item.title}
             </p>
