9a6b74d53338904b3487fa82e418cf4639aadfef
John Jang <jscolbe9675@gmail.com>
2024-09-02 14:32:27 -0400

    added text change

--- src/components/footer2.jsx
+++ src/components/footer2.jsx
@@ -6,7 +6,7 @@ export default function OtherMenu() {
   return (
     <div className="md:flex md:items-center md:space-x-40 justify-center space-y-10 md:space-y-0 mx-8 md:mx-0">
       <Link to="/">
-        <div className="flex items-center p-5 rounded-2xl gap-2 text-blue-500 bg-transparent hover:bg-gradient-to-br hover:from-blue-400 hover:to-transparent transition-all duration-2000 ease-in-out">
+        <div className="flex items-center p-5 rounded-2xl gap-2 text-blue-500 bg-transparent hover:text-white hover:bg-gradient-to-br hover:from-blue-400 hover:to-transparent transition-all duration-2000 ease-in-out">
           <button>
             <HiMiniArrowLongLeft />
           </button>
@@ -14,7 +14,7 @@ export default function OtherMenu() {
         </div>
       </Link>
       <a href="mailto:jscolbe9675@gmail.com">
-        <div className="flex items-center p-5 rounded-2xl gap-2 text-blue-500 bg-transparent hover:bg-gradient-to-br hover:from-blue-400 hover:to-transparent transition-all duration-2000 ease-in-out">
+        <div className="flex items-center p-5 rounded-2xl gap-2 text-blue-500 bg-transparent hover:text-white hover:bg-gradient-to-br hover:from-blue-400 hover:to-transparent transition-all duration-2000 ease-in-out">
           <button>Contact me for the full detail of the project</button>
           <button>
             <HiMiniArrowRight />
