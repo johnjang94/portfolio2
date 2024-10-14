4819ad823c838d61e6d0961c4c98f4919d4af9fd
John Jang <jscolbe9675@gmail.com>
2024-10-14 13:14:26 -0400

    modified sakhi content

--- src/pages/design/sakhi/impact.jsx
+++ src/pages/design/sakhi/impact.jsx
@@ -4,7 +4,9 @@ export default function Impact() {
       <h3 className="text-xl uppercase">Impact</h3>
       <div className="md:mx-20 my-3 space-y-5">
         <p className="font-semibold">
-          In this phase (Phase 1), we noticed that...
+          Since this was Phase 0, we have not actually been able to launch the
+          platform in this phase. On the other hand, the participants who joined
+          us in usability testing have shown the following performances:
         </p>
         <ul className="px-10 space-y-3">
           <p>
@@ -12,10 +14,11 @@ export default function Impact() {
             <span className="text-blue-500">3.5%</span> with new pages we
             created.
           </p>
-          <p>
-            <span className="text-blue-500">50%</span> of the participants have
-            said that they are interested in learning more and trying out the
-            clothes they see from the website.
+          <li>Screen time</li>
+          <p className="px-5">
+            the average time that users stay engaged on the website increased{" "}
+            {/* */}
+            <span className="text-blue-500">from 2 minutes to 5 minutes</span>.
           </p>
         </ul>
       </div>
