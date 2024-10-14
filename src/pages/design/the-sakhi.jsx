7cee069769aeac7076b375635c43d5171289c5f3
John Jang <jscolbe9675@gmail.com>
2024-10-14 13:14:26 -0400

    modified sakhi content

--- src/pages/design/the-sakhi.jsx
+++ src/pages/design/the-sakhi.jsx
@@ -122,6 +122,9 @@ export default function SAKHI() {
               <div>
                 <Insights />
               </div>
+              <div>
+                <UserPersona />
+              </div>
               <div id={2}>
                 <Opportunity />
               </div>
