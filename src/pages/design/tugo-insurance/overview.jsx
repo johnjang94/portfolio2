3d4e00276f343196b2b29d0cd4c9dac725f9f2c2
John Jang <jscolbe9675@gmail.com>
2024-10-15 14:00:39 -0400

    modified the content for TuGo Insurance

--- src/pages/design/tugo-insurance/overview.jsx
+++ src/pages/design/tugo-insurance/overview.jsx
@@ -15,6 +15,7 @@ export default function Overview() {
         </div>
         <div>
           <p className="font-semibold">Mission Objective</p>
+          <p className="font-semibold">Mission Objective</p>
           <div className="p-5">
             <p>
               In this phase, the goal was to raise the click-through-rate (CTR)
@@ -46,9 +47,26 @@ export default function Overview() {
                 3 out of 5 potential travelers
               </span>{" "}
               have successfully explored the insurance products and ordered
+              insurance packages. In this phase, the goal was to raise the
+              click-through-rate (CTR) to{" "}
+              <span className="text-blue-500">6%</span>.
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="font-semibold">Outcome</p>
+          <div className="p-5 space-y-3">
+            <p>
+              About{" "}
+              <span className="text-blue-500">
+                3 out of 5 potential travelers
+              </span>{" "}
+              have successfully explored the insurance products and ordered
               insurance packages.
             </p>
             <p>
+              Unfortunately, we did not have an adequate amount of time to find
+              out how many views we reached per hour at this point.
               Unfortunately, we did not have an adequate amount of time to find
               out how many views we reached per hour at this point.
               Unfortunately, we did not have an adequate amount of time to find
