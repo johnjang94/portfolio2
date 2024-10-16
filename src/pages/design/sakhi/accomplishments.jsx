54a5516ecdff1f3ebdb9b7847cdb73355d422f07
John Jang <jscolbe9675@gmail.com>
2024-10-16 11:59:29 -0400

    modified content for Sakhi

--- src/pages/design/sakhi/accomplishments.jsx
+++ src/pages/design/sakhi/accomplishments.jsx
@@ -4,21 +4,14 @@ export default function Accomplishments() {
       <h3 className="text-xl uppercase">Accomplishments</h3>
       <div className="md:mx-20 my-3">
         <ul className="space-y-5 list-disc">
-          <p>Please stay tuned until further update.</p>
-          {/* <p>
-            We have successfully reached our target by offering the following
-            differentiated services:
+          <p>
+            Although it is not that we have reached the sale of $ 10,000, but we
+            have raised{" "}
+            <span className="text-blue-500">
+              the click-through rate (CTR) of 3.5%
+            </span>{" "}
+            as the initial kick-off.
           </p>
-          <li className="px-10">
-            Shoppers can place an order in{" "}
-            <span className="text-blue-500">maximum 3-clicks</span>
-          </li>
-          <li className="px-10">
-            We reduced the amount of time that the shoppers use in browsing and
-            comparison different products by{" "}
-            <span className="text-blue-500">60%</span> of time, which is an
-            average of <span className="text-blue-500">5 minutes</span>
-          </li> */}
         </ul>
       </div>
     </div>
--- src/pages/design/sakhi/solution.jsx
+++ src/pages/design/sakhi/solution.jsx
@@ -4,40 +4,27 @@ export default function Solutions() {
       <h3 className="text-xl uppercase">Solution</h3>
       <div className="md:mx-20 my-3 space-y-10">
         <div className="space-y-3">
-          <p className="font-semibold">Iteration 1 | Simplified Navigation</p>
+          <p className="font-semibold">Iteration 1 | Perceptive Navigation</p>
           <ul className="px-10 space-y-3">
-            <p>We have redesigned the navigation in two different ways:</p>
-            <li className="px-8">
-              We have simplified overall navigation within the platform itself.
-            </li>
-            <p className="px-14 text-gray-400">
-              i.e. we introduce new arrivals first, then allow shoppers to
-              browse for more
+            <p>
+              We created a navigation menu that reflects the brand&#39;s colors
+              and designed a seamless shopping experience requiring no further
+              assistance.
             </p>
-            <li className="px-8">
-              We came up with filter function that displays lots of varieties.
-            </li>
           </ul>
           <div className="p-3 bg-white rounded-3xl">
-            <img
-              src=""
-              alt="Final design for quick compare"
-              className="text-3xl"
-            />
+            <img src="" alt="" className="text-3xl" />
           </div>
         </div>
         <div className="space-y-3">
           <p className="font-semibold">Iteration 2 | Friendly Visuals</p>
           <p className="px-10">
-            We used friendly terminologies and simple visuals to promote
-            eye-catching shopping experience
+            Since we are inviting shoppers unfamiliar with Indian products as
+            well, we used friendly terminologies and simple visuals to create an
+            eye-catching shopping experience.
           </p>
           <div className="p-3 bg-white rounded-3xl">
-            <img
-              src=""
-              alt="Final design for product detail page"
-              className="text-3xl"
-            />
+            <img src="" alt="" className="text-3xl" />
           </div>
         </div>
       </div>
