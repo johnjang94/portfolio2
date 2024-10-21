b15ca56ca761a4891d94f25b1b391bde632bb537
John Jang <jscolbe9675@gmail.com>
2024-10-21 12:19:33 -0400

    modified next steps for operate-ease

--- src/pages/design/operate/next-steps.jsx
+++ src/pages/design/operate/next-steps.jsx
@@ -3,7 +3,14 @@ export default function NextSteps() {
     <div>
       <h3 className="text-xl uppercase">Next steps</h3>
       <div className="md:mx-20 my-3 space-y-3">
-        <p>OperateEase has a potential to be expanded to smaller mobile-use.</p>
+        <p>OperateEase teams will finish high-fi design in near future</p>
+        <p>
+          OperateEase will start developing the product in the next couple of
+          weeks
+        </p>
+        <p>
+          OperateEase design team will continue to work on the mobile designs
+        </p>
       </div>
     </div>
   );
