c2eda17e1f8dac4d2a8cb111786e5554869ae716
John Jang <jscolbe9675@gmail.com>
2024-10-21 11:18:37 -0400

    modified sakhi content on research, solution, and takeaways

--- src/pages/design/the-sakhi.jsx
+++ src/pages/design/the-sakhi.jsx
@@ -18,6 +18,7 @@ import Impact from "./sakhi/impact";
 import { sakhi } from "../../utils/sakhi";
 import Retrospective from "./sakhi/retrospective";
 import NextSteps from "./sakhi/next-steps";
+import NextSteps from "./sakhi/next-steps";
 
 export default function SAKHI() {
   const [currentSection, setCurrentSection] = useState(1);
@@ -132,6 +133,9 @@ export default function SAKHI() {
               </div>
               <div>
                 <Retrospective />
+              </div>
+              <div>
+                <NextSteps />
               </div>
               <div>
                 <NextSteps />
