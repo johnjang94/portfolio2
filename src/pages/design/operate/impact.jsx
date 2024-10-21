aef2715ba561575d3fb6639fb861d5b72c92b85e
John Jang <jscolbe9675@gmail.com>
2024-10-21 12:10:07 -0400

    oe updating the impact

--- src/pages/design/operate/impact.jsx
+++ src/pages/design/operate/impact.jsx
@@ -5,18 +5,7 @@ export default function Impact() {
       <div className="md:mx-20 my-3 space-y-5">
         <p className="font-semibold">Based on the usability testing:</p>
         <ul className="px-5 list-disc space-y-3">
-          <li>Task success rate</li>
-          <p className="px-5">
-            <span className="text-blue-500">85%</span> of participants{" "}
-            <span className="text-blue-500">have successfully navigated</span>{" "}
-            themselves from the home page to the cart page.
-          </p>
-          <li>Screen time</li>
-          <p className="px-5">
-            the average time that users stay engaged on the website increased{" "}
-            {/* */}
-            <span className="text-blue-500">from 2 minutes to 5 minutes</span>.
-          </p>
+          <p>please stay tuned for further update.</p>
         </ul>
       </div>
     </div>
