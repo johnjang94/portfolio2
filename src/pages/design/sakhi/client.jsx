515a41cfe6561a9d0c1dcd0a980e3190d17c4d54
John Jang <jscolbe9675@gmail.com>
2024-10-30 16:44:28 -0400

    modified client introduction for the sakhi

--- src/pages/design/sakhi/client.jsx
+++ src/pages/design/sakhi/client.jsx
@@ -5,8 +5,9 @@ export default function Client() {
       <div className="md:mx-20 space-y-5">
         <p className="font-semibold">The Sakhi</p>
         <p className="px-10">
-          A specialized e-commerce platform that offers unique and traditional
-          products for extraordinary life events such as wedding, festival, etc.
+          A specialized e-commerce platform (a start-up) that offers unique and
+          traditional products for extraordinary life events such as wedding,
+          festival, etc.
         </p>
       </div>
     </div>
