7c52f565c4924065e9c1abd09ebbd557030a6220
John Jang <jscolbe9675@gmail.com>
2024-10-31 13:59:06 -0400

    adding sketches and mock-ups to televu study case

--- src/pages/design/televu/next-steps.jsx
+++ src/pages/design/televu/next-steps.jsx
@@ -4,13 +4,6 @@ export default function NextSteps() {
       <h3 className="text-xl uppercase">Next Steps</h3>
       <div className="md:mx-20 my-3">
         <p className="font-semibold">
-          We <span className="text-red-500">have not had a chance</span> to test
-          the usability of our designs with the clinicians
-        </p>
-        <p className="p-5">
-          unfortunately, the clinicians are usually extremely busy
-        </p>
-        <p className="font-semibold">
           Next phase team{" "}
           <span className="text-blue-500">may continue to design</span> the full
           mobile experience
