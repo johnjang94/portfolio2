3928ecdaeab4c758434e9f7c62fe00c6f6ed53e3
John Jang <jscolbe9675@gmail.com>
2024-10-31 08:16:52 -0400

    modified the retrospective for operationease

--- src/pages/design/operate/retrospective.jsx
+++ src/pages/design/operate/retrospective.jsx
@@ -3,23 +3,14 @@ export default function Retrospective() {
     <div>
       <h3 className="text-xl uppercase">Looking back...</h3>
       <div className="md:mx-20 my-3 space-y-5">
-        <p className="font-semibold">
-          This was my first SaaS project. I only used to assume that SaaS
-          project is oriented for the office-use, but I came to realize that
-          SaaS is helpful when it comes to automating and systemizing the
-          actions that repeat periodically.
-        </p>
         <ul className="px-5 space-y-3">
           <li className="text-blue-500">
-            Best Lesson 1 | SaaS for B2B is complex
+            Best Lesson 1 | The Subscription Model
           </li>
           <p className="px-5">
-            In this project, all the services were offered as a package. On the
-            other hand, I did not have any idea what exactly it means. It was a
-            new discovery of how different systems can collaborate and be
-            offered as a package. In addition, I was amazed to find out how
-            complex the interactivity of different activities occur at the same
-            time.
+            In this project, all the services were offered as a package. As I am
+            not a business-person yet, it was a good opportunity to find out
+            what a business model looks like and how it works in reality.
           </p>
           <li className="text-blue-500">
             Best Lesson 2 | Discovery of Unknown
