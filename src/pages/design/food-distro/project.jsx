d81ef6f38223be9040adbb3a385dfcc0d16ae4a3
John Jang <jscolbe9675@gmail.com>
2024-10-31 08:27:45 -0400

    added reason for food-distro

--- src/pages/design/food-distro/project.jsx
+++ src/pages/design/food-distro/project.jsx
@@ -9,6 +9,20 @@ export default function Project() {
           individuals / households in suburban / rural communities by improving
           accessibility to social services.
         </p>
+        <ul className="list-disc space-y-3 py-10">
+          <p className="font-bold">Why this project?</p>
+          <li className="mx-5">
+            While contemplating on how to increase awareness among the locals, I
+            learned the importance and a few techniques of marketing
+          </li>
+          <li className="mx-5">
+            This was a good opportunity to think about how to foster the
+            communities that are in need of help in another way:
+          </li>
+          <p className="text-center py-10 text-blue-500 text-2xl">
+            &#34;Sharing is Caring&#34;
+          </p>
+        </ul>
       </div>
     </div>
   );
