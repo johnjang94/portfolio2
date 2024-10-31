c20d0e8941ee1fcad45345ca3c2b4780d256f250
John Jang <jscolbe9675@gmail.com>
2024-10-31 14:46:42 -0400

    modified operationease for client

--- src/pages/design/operate/client.jsx
+++ src/pages/design/operate/client.jsx
@@ -9,6 +9,14 @@ export default function Client() {
           reducing stress, and enhancing customer experiences in the areas of
           restaurant, lodging, and finance.
         </p>
+        <div className="space-y-3 py-10">
+          <p className="font-bold">Why this project?</p>
+          <li className="mx-5">
+            Because this project was all about how to improve the quality of
+            management, I got a chance to think about how to make the work flow
+            smarter and better
+          </li>
+        </div>
       </div>
     </div>
   );
