e1e9db7276dd9f6de01c7af24944c011db5eabe8
John Jang <jscolbe9675@gmail.com>
2024-11-01 11:46:06 -0400

    made size changes on my resume to make it responsive

--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -1,15 +1,13 @@
 export default function Resume() {
   return (
     <section>
-      <p className="text-center mt-10 text-2xl">
+      <p className="text-center mt-10 md:text-2xl">
         The following is my sample resume.
       </p>
       <div className="my-10">
         <iframe
-          className="pdf mx-auto"
+          className="pdf mx-auto w-5/6 h-[450px]"
           src="/product_designer.pdf"
-          width="800"
-          height="500"
         ></iframe>
       </div>
       <div className="flex items-center text-center gap-1 justify-center">
