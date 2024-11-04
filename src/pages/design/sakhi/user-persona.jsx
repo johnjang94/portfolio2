09fce302dfcd4bbbb0cc0569e5c5532c79b474d9
John Jang <jscolbe9675@gmail.com>
2024-11-04 14:00:13 -0500

    storyline change for the sakhi

--- src/pages/design/sakhi/user-persona.jsx
+++ src/pages/design/sakhi/user-persona.jsx
@@ -3,20 +3,19 @@ import { sakhi } from "../../../utils/sakhi";
 export default function UserPersona() {
   return (
     <div className="space-y-5">
-      <h3 className="text-xl uppercase">User Persona</h3>
+      <p className="text-xl font-semibold text-center">
+        What makes them to be withdrawn?
+      </p>
       <div className="md:mx-20 space-y-5">
         <div className="space-y-3">
-          <p>
-            In addition to the competitive analysis, we needed the voices on
-            what specific issues the potential shoppers were currently
-            experiencing from other existing brands:
-          </p>
           <ul className="text-red-500 text-center py-10 space-y-3">
             <li>
               &#34;Many Indian platforms have a complex navigation menu or
               filter.&#34;
             </li>
-            <li>&#34;Not many brands offer comparison among products.&#34;</li>
+            <li>
+              &#34;Not many Indian brands offer comparison among products.&#34;
+            </li>
             <li>
               &#34;Visuals that they have do not speak enough messages.&#34;
             </li>
