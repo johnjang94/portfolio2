d2366cf819cb8bd6f0aeb1c2241384915bbe743d
John Jang <jscolbe9675@gmail.com>
2024-11-04 00:24:10 -0500

    fixed the contents for operationease

---
+++ src/pages/design/operate/background.jsx
@@ -0,0 +1,11 @@
+export default function Background() {
+  return (
+    <div className="space-y-5">
+      <h3 className="text-xl uppercase">Background</h3>
+      <div className="md:mx-20 space-y-5">
+        <p className="text-xl">Why did this issue catch your eye?</p>
+        <p className="px-10">Because...</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/design/operate/motive.jsx
@@ -0,0 +1,13 @@
+export default function Motive() {
+  return (
+    <div>
+      <h3 className="text-xl uppercase">Motive</h3>
+      <ul className="p-5 md:px-28 space-y-3">
+        <li>
+          Many hoteliers want to migrate to digital platform to simplify and
+          improve their workflow.
+        </li>
+      </ul>
+    </div>
+  );
+}
